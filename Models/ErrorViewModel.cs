e68db8e3516fc559a6d5af3256402211219214ab
Carlos Eduardo <cbarboza@gmail.com>
2018-03-08 20:17:18 -0300

    :tada: Initial Commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_chalenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
